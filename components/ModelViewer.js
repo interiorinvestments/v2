52b1a32de77d86c45404dd100168031855c500b5
Alex Matseshe <amatseshe@gmail.com>
2020-05-21 11:05:54 -0500

    Add model-vewier

--- components/ModelViewer.js
+++ components/ModelViewer.js
@@ -2,10 +2,17 @@ import '@google/model-viewer';
 
 import PropTypes from 'prop-types';
 
-const ModelViewer = ({ model }) => {
-  console.log(model);
-  return <h1>model</h1>;
-};
+const ModelViewer = ({ model }) => (
+  <model-viewer
+    src={model.model}
+    ar
+    alt={model.name}
+    auto-rotate
+    camera-controls
+    ios-src={model.iosModel}
+    style={{ height: 'auto', width: '100%' }}
+  />
+);
 
 ModelViewer.propTypes = {
   model: PropTypes.object.isRequired,
